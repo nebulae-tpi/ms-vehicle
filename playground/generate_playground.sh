3e09b9f19a7a2a98d8f8b9a101af206a4c50e215
Daniel Tobon <daniel.tobon@nebulae.com.co>
2019-04-05 10:37:43 -0500

    Se agrega driver-gateway junto con el metodo de consulta de la membresia del vehiculo

--- playground/generate_playground.sh
+++ playground/generate_playground.sh
@@ -3,5 +3,8 @@
 # FrontEnd - EMI composition
 nebulae compose-ui development --shell-type=FUSE2_ANGULAR --shell-repo=https://github.com/nebulae-tpi/emi --frontend-id=emi --output-dir=emi  --setup-file=../etc/mfe-setup.json
 
-# API - GateWay composition
+# API - EMI GateWay composition
 nebulae compose-api development --api-type=NEBULAE_GATEWAY --api-repo=https://github.com/nebulae-tpi/emi-gateway --api-id=emi-gateway --output-dir=emi-gateway  --setup-file=../etc/mapi-setup.json
+
+# API - DRIVER GateWay composition
+nebulae compose-api development --api-type=NEBULAE_GATEWAY --api-repo=https://github.com/nebulae-tpi/driver-gateway --api-id=driver-gateway --output-dir=driver-gateway  --setup-file=../etc/mapi-setup.json
