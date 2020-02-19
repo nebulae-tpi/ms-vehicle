c444d4178cb31edd69c36825b7ebfff483df52aa
juansantane <juan.santa@nebulae.com.co>
2020-02-19 10:38:42 -0500

    changes made to send to master

--- api/driver-gateway/graphql/vehicle/resolvers.js
+++ api/driver-gateway/graphql/vehicle/resolvers.js
@@ -10,6 +10,7 @@ const { handleError$ } = require('../../tools/GraphqlResponseTools');
 const INTERNAL_SERVER_ERROR_CODE = 1;
 const PERMISSION_DENIED_ERROR_CODE = 2;
 
+
 function getResponseFromBackEnd$(response) {
   return of(response).pipe(
     map(resp => {
